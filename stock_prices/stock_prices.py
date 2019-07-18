fef4782d703d24b9aaaf08af60c3d4c20d4a8cbb
Garrett Weems <gwgraphicdesign@gmail.com>
2019-07-17 20:01:27 -0400

    🍻 stock prices algorithm

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,16 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    max = 0
+    for i, price in enumerate(prices):
+        for j in range(i + 1, len(prices)):
+            profit = prices[j] - price
+            if max == 0 or profit > max:
+                max = profit
+    return max
 
 
-if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
-
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+stocks = [1050, 270, 1540, 3800, 2]
+print(find_max_profit(stocks))
